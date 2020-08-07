f519d873d5f927849033b0a3f07f4061690a511e
Yacine ch <chix88@gmail.com>
2020-08-07 17:09:26 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SQL_Tester
+{
+    partial class frmTester
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdRecord = new System.Windows.Forms.DataGridView();
+            this.btnExcute = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.txtCommand = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecord)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdRecord
+            // 
+            this.grdRecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdRecord.Location = new System.Drawing.Point(12, 12);
+            this.grdRecord.Name = "grdRecord";
+            this.grdRecord.Size = new System.Drawing.Size(776, 242);
+            this.grdRecord.TabIndex = 0;
+            // 
+            // btnExcute
+            // 
+            this.btnExcute.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcute.Location = new System.Drawing.Point(627, 279);
+            this.btnExcute.Name = "btnExcute";
+            this.btnExcute.Size = new System.Drawing.Size(161, 33);
+            this.btnExcute.TabIndex = 1;
+            this.btnExcute.Text = "Execute Statement";
+            this.btnExcute.UseVisualStyleBackColor = true;
+            this.btnExcute.Click += new System.EventHandler(this.BtnExcute_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(647, 353);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Record count";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(725, 353);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(16, 16);
+            this.lblCount.TabIndex = 3;
+            this.lblCount.Text = "0";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtCommand
+            // 
+            this.txtCommand.Location = new System.Drawing.Point(13, 286);
+            this.txtCommand.Multiline = true;
+            this.txtCommand.Name = "txtCommand";
+            this.txtCommand.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommand.Size = new System.Drawing.Size(538, 122);
+            this.txtCommand.TabIndex = 4;
+            // 
+            // frmTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCommand);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExcute);
+            this.Controls.Add(this.grdRecord);
+            this.Name = "frmTester";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmTester_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecord)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdRecord;
+        private System.Windows.Forms.Button btnExcute;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TextBox txtCommand;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQL_Tester
+{
+    public partial class frmTester : Form
+    {
+        OleDbConnection conn = new OleDbConnection();
+        private OleDbCommand command = new OleDbCommand();
+        private OleDbDataAdapter dataAdapter = new OleDbDataAdapter();
+        private DataTable dt;
+
+        public frmTester()
+        {
+            InitializeComponent();
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\DB\Books.accdb;Persist Security Info=False;
+            ";
+        }
+
+        private void FrmTester_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void BtnExcute_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            dt.Clear();
+            conn.Open();
+            dataAdapter.SelectCommand = new OleDbCommand(txtCommand.Text,conn);
+
+            dataAdapter.Fill(dt);
+            grdRecord.DataSource = dt;
+            
+            dataAdapter.Dispose();
+            conn.Close();
+
+        }
+    }
+}
